3f2728e199122a62b95e8240d0b920d8cadb4d0f
arthur <artyr007@gmail.com>
2024-03-07 21:00:10 +0500

    Add form functionality to add books to the array

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 const myLibrary = [];
-
-document.get
+const submitButton = document.getElementById("submit-button");
 
 function Book(name, author, pages, read, year, rating) {
   this.name = name;
@@ -14,3 +13,18 @@ function Book(name, author, pages, read, year, rating) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let form = document.getElementById("book-form");
+  let name = form.elements["name"].value;
+  let author = form.elements["author"].value;
+  let pages = form.elements["pages"].value;
+  let readStatus = form.elements["read-status"].value;
+  let year = form.elements["year"].value;
+  let rating = form.elements["rating"].value;
+
+  const bookData = new Book(name, author, pages, readStatus, year, rating);
+  addBookToLibrary(bookData);
+});
