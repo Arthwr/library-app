fd6e72bc1cd82357e02d1f451947ea6a31cadc4a
arthur <artyr007@gmail.com>
2024-03-07 22:52:05 +0500

    Complete functionality of form
    Add sticky position to form element in case if table height is higher than viewport size
    Add dynamically assigned buttons to each table row of added book

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 const myLibrary = [];
-const submitButton = document.getElementById("submit-button");
+const bookPropertyOrder = ["name", "author", "pages", "year", "rating", "read"];
 
-function Book(name, author, pages, read, year, rating) {
+const submitButton = document.getElementById("submit-button");
+const tableBody = document.querySelector("tbody");
+
+function Book(name, author, pages, year, read, rating) {
   this.name = name;
   this.author = author;
   this.pages = pages;
@@ -12,6 +15,40 @@ function Book(name, author, pages, read, year, rating) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  createAndPopulateTableRow(book);
+}
+
+function createAndPopulateTableRow(book) {
+  // Create book data row
+  const tableRow = document.createElement("tr");
+  tableBody.appendChild(tableRow);
+
+  // Populate book data row
+  for (let key of bookPropertyOrder) {
+    const tableData = document.createElement("td");
+    tableRow.appendChild(tableData);
+    tableData.textContent = book[key];
+  }
+
+  // Create action buttons row
+  const tableActions = document.createElement("td");
+  const actionsWrapper = document.createElement("div");
+  actionsWrapper.classList.add("actions-wrapper");
+  tableRow.appendChild(tableActions);
+  tableActions.appendChild(actionsWrapper);
+
+  // Create buttons
+  const actionButtons = [
+    { text: "Change status", action: () => console.log("Clicked") },
+    { text: "Remove", action: () => console.log("Clicked") },
+  ];
+
+  actionButtons.forEach((buttonData) => {
+    const button = document.createElement("a");
+    button.textContent = buttonData.text;
+    button.addEventListener("click", buttonData.action);
+    actionsWrapper.appendChild(button);
+  });
 }
 
 submitButton.addEventListener("click", (e) => {
@@ -21,10 +58,12 @@ submitButton.addEventListener("click", (e) => {
   let name = form.elements["name"].value;
   let author = form.elements["author"].value;
   let pages = form.elements["pages"].value;
-  let readStatus = form.elements["read-status"].value;
   let year = form.elements["year"].value;
   let rating = form.elements["rating"].value;
+  let readStatus = form.elements["read-status"].checked
+    ? "Read"
+    : "Not read yet";
 
-  const bookData = new Book(name, author, pages, readStatus, year, rating);
+  const bookData = new Book(name, author, pages, year, readStatus, rating);
   addBookToLibrary(bookData);
 });
