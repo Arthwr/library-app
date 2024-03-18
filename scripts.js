2f296eefa2626768294a879495c44562003a51e3
arthur <artyr007@gmail.com>
2024-03-19 00:50:08 +0500

    Refactor library functionality into ES6 Class syntax
    Also clean up code structure & move static data into class component to encapsulate it

--- scripts.js
+++ scripts.js
@@ -1,110 +1,119 @@
-const mockData = [
-  {
-    name: "The Lord of The Rings",
-    author: "J.R.R. Tolkien",
-    pages: "1178",
-    year: "1954",
-    read: "Read",
-    rating: "5",
-  },
-  {
-    name: "The Witcher Trilogy",
-    author: "Andrzej Sapkowski",
-    pages: "3017",
-    year: "1986",
-    read: "Read",
-    rating: "5",
-  },
-];
-const myLibrary = [];
-const bookPropertyOrder = ["name", "author", "pages", "year", "rating", "read"];
-
-const submitButton = document.getElementById("submit-button");
-const tableBody = document.querySelector("tbody");
-
-function Book(name, author, pages, year, read, rating) {
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.year = year;
-  this.rating = rating;
-}
-
-Book.prototype.toggleStatus = function () {
-  this.read = this.read === "Read" ? "Not read yet" : "Read";
-};
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-  const index = myLibrary.indexOf(book);
-  createAndPopulateTableRow(book, index);
-}
-
-function createAndPopulateTableRow(book, index) {
-  // Create book data row
-  const tableRow = document.createElement("tr");
-  tableBody.appendChild(tableRow);
-
-  // Populate book data row
-  for (let key of bookPropertyOrder) {
-    const tableData = document.createElement("td");
-    tableRow.appendChild(tableData);
-    tableData.textContent = book[key];
+class Book {
+  constructor(name, author, pages, year, read, rating) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.year = year;
+    this.rating = rating;
   }
+  toggleStatus() {
+    this.read = this.read === "Read" ? "Not read yet" : "Read";
+  }
+}
 
-  // Create action buttons row
-  const tableActions = document.createElement("td");
-  const actionsWrapper = document.createElement("div");
-  actionsWrapper.classList.add("actions-wrapper");
-  tableRow.appendChild(tableActions);
-  tableActions.appendChild(actionsWrapper);
-
-  // Create buttons and assign actions
-  const actionButtons = [
+class Library {
+  static mockData = [
     {
-      text: "Change status",
-      action: function () {
-        book.toggleStatus();
-        const readCell = tableRow.querySelector("td:nth-child(6)");
-        readCell.textContent = book.read;
-      },
+      name: "The Lord of The Rings",
+      author: "J.R.R. Tolkien",
+      pages: "1178",
+      year: "1954",
+      read: "Read",
+      rating: "5",
     },
     {
-      text: "Remove",
-      action: function () {
-        myLibrary.splice(index, 1);
-        tableRow.remove();
-      },
+      name: "The Witcher Trilogy",
+      author: "Andrzej Sapkowski",
+      pages: "3017",
+      year: "1986",
+      read: "Read",
+      rating: "5",
     },
   ];
 
-  actionButtons.forEach((buttonData) => {
-    const button = document.createElement("a");
-    button.textContent = buttonData.text;
-    button.addEventListener("click", buttonData.action);
-    actionsWrapper.appendChild(button);
-  });
+  // prettier-ignore
+  constructor() {
+    this.bookCollection = [];
+    this.tableBody = document.querySelector("tbody");
+    this.bookPropertyOrder = ["name", "author", "pages", "year", "rating", "read"];
+  }
+
+  addBook(book) {
+    this.bookCollection.push(book);
+    const index = this.bookCollection.indexOf(book);
+    this.createAndPopulateTableRow(book, index);
+  }
+
+  createAndPopulateTableRow(book, index) {
+    // Create book data row
+    const tableRow = document.createElement("tr");
+    this.tableBody.appendChild(tableRow);
+
+    // Populate book data row
+    for (let key of this.bookPropertyOrder) {
+      const tableData = document.createElement("td");
+      tableRow.appendChild(tableData);
+      tableData.textContent = book[key];
+    }
+
+    // Create action buttons row
+    const tableActions = document.createElement("td");
+    const actionsWrapper = document.createElement("div");
+    actionsWrapper.classList.add("actions-wrapper");
+    tableRow.appendChild(tableActions);
+    tableActions.appendChild(actionsWrapper);
+
+    // Create buttons and assign actions
+    const actionButtons = [
+      {
+        text: "Change status",
+        action: () => {
+          book.toggleStatus();
+          const readCell = tableRow.querySelector("td:nth-child(6)");
+          readCell.textContent = book.read;
+        },
+      },
+      {
+        text: "Remove",
+        action: () => {
+          this.bookCollection.splice(index, 1);
+          tableRow.remove();
+        },
+      },
+    ];
+
+    actionButtons.forEach((buttonData) => {
+      const button = document.createElement("a");
+      button.textContent = buttonData.text;
+      button.addEventListener("click", buttonData.action);
+      actionsWrapper.appendChild(button);
+    });
+  }
+
+  // Populate library with initial mock data
+  init() {
+    Library.mockData.forEach((book) => {
+      const myBook = new Book(
+        book.name,
+        book.author,
+        book.pages,
+        book.year,
+        book.read,
+        book.rating
+      );
+      this.addBook(myBook);
+    });
+  }
 }
 
-// Populate library with initial mock data
-function init() {
-  mockData.forEach((book) => {
-    const myBook = new Book(
-      book.name,
-      book.author,
-      book.pages,
-      book.year,
-      book.read,
-      book.rating
-    );
-    addBookToLibrary(myBook);
-  });
-}
-
-init();
+// Create and init new library
+const myLibrary = new Library();
+myLibrary.init();
 
 // Retrieve form data and populate library
+const submitButton = document.getElementById("submit-button");
+
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
 
@@ -119,5 +128,5 @@ submitButton.addEventListener("click", (e) => {
     : "Not read yet";
 
   const bookData = new Book(name, author, pages, year, readStatus, rating);
-  addBookToLibrary(bookData);
+  myLibrary.addBook(bookData);
 });
